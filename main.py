78d0aaf289f53b4797add6bee62f2d4b927c5935
Fanani M. Ihsan <fanani.mi@gmail.com>
2020-04-29 22:37:58 +0700

    reffactor

--- main.py
+++ main.py
@@ -13,107 +13,124 @@ class MifareTools(Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MifareTools, self).__init__(parent)
         self.setupUi(self)
-        self._register_signal()
+        self.register_signal()
 
         # attribute registration
+        self.is_picc_connect = False
         self.current_connection = None
         self.reader_model = QtGui.QStandardItemModel()
 
         # finally we render the user interface
-        self._init_ui()
+        self.init_ui()
 
-    def on_click_button(self):
-        btnID = self.sender().objectName()
-        if btnID == 'btnReloadReader':
-            self._reload_readers()
-            return
-        elif btnID == "btnConnetPICC":
-            self._connect()
-            return
-        elif btnID == "btnAuthKeyA":
-            return
-        elif btnID == "btnAuthKeyB":
-            print("btnAuthKeyB")
-            return
-        elif btnID == "btnFactoryKeyA":
-            for i in range(0, 6):
-                getattr(self, 'txtKeyA%d' % i).setText("FF")
-            return
-        elif btnID == "btnFactoryKeyB":
-            for i in range(0, 6):
-                getattr(self, 'txtKeyB%d' % i).setText("FF")
-            return
-
-    def write_statusbar(self, message, color='green'):
-        self.statusbar.setStyleSheet("color: %s;" % color)
-        self.statusbar.showMessage(message)
+    def __del__(self):
+        if self.is_picc_connect:
+            self.disconnect_picc()
 
     # --------------------------------------------------------------------------------
     # ********************* All functions shown to user is here *********************|
     # --------------------------------------------------------------------------------
-    def _init_ui(self):
+    def init_ui(self):
         # set combobox
         self.cmbReader.setModel(self.reader_model)
-        self._reload_readers()
+        self.reload_readers()
 
     # --------------------------------------------------------------------------------
     # ****************** All signals must register on this section ******************|
     # --------------------------------------------------------------------------------
-    def _register_signal(self):
-        self.btnReloadReader.clicked.connect(self.on_click_button)
-        self.btnConnetPICC.clicked.connect(self.on_click_button)
-        self.btnAuthKeyA.clicked.connect(self.on_click_button)
-        self.btnAuthKeyB.clicked.connect(self.on_click_button)
-        self.btnFactoryKeyA.clicked.connect(self.on_click_button)
-        self.btnFactoryKeyB.clicked.connect(self.on_click_button)
+    def register_signal(self):
+        # clicked signal
+        self.btnReloadReader.clicked.connect(self.on_click)
+        self.btnConnetPICC.clicked.connect(self.on_click)
+        self.btnAuthKeyA.clicked.connect(self.on_click)
+        self.btnAuthKeyB.clicked.connect(self.on_click)
+        self.btnFactoryKeyA.clicked.connect(self.on_click)
+        self.btnFactoryKeyB.clicked.connect(self.on_click)
+        # comoBox changed index
+        self.cmbReader.currentIndexChanged.connect(self.on_combobox_index_changed)
 
-    def _reload_readers(self):
+
+    # --------------------------------------------------------------------------------
+    # *************************** Signal callback is here ***************************|
+    # --------------------------------------------------------------------------------
+    def on_combobox_index_changed(self):
+        sender = self.sender().objectName()
+        if sender == self.cmbReader.objectName():
+            cmb_idx = self.cmbReader.currentIndex()
+            reader_obj = self.reader_model.item(cmb_idx)
+            if hasattr(reader_obj, 'data'):
+                self.current_connection = reader_obj.data().createConnection()
+                self.groupBoxPICCC.setEnabled(True)
+                self.write_statusbar("Reader connected")
+            else:
+                self.groupBoxPICCC.setEnabled(False)
+                self.write_statusbar("Reader not connected", "blue")
+            return
+
+    def on_click(self):
+        sender = self.sender().objectName()
+        if sender == self.btnReloadReader.objectName():
+            self.reload_readers()
+            return
+        elif sender == self.btnConnetPICC.objectName():
+            if not self.is_picc_connect:
+                self.connect_picc()
+            else:
+                self.disconnect_picc()
+
+    # --------------------------------------------------------------------------------
+    # *************************** Helper function is here ***************************|
+    # --------------------------------------------------------------------------------
+    def connect_picc(self):
+        try:
+            self.current_connection.connect()
+            uid = self.transmit(command.UID)
+            atr = toHexString(self.current_connection.getATR())
+            self.txtUID.setText(uid)
+            self.txtATR.setText(atr)
+            self.btnConnetPICC.setText("Disconnect")
+            self.tabMain.setEnabled(True)
+            self.is_picc_connect = True
+            self.write_statusbar("Card connected")
+        except NoCardException as e:
+            self.write_statusbar(str(e), "red")
+        except CardConnectionException as e:
+            self.reload_readers()
+            self.write_statusbar(str(e), "red")
+
+    def disconnect_picc(self):
+        try:
+            self.current_connection.disconnect()
+        except CardConnectionException:
+            pass
+
+        uid = ""
+        atr = ""
+        self.txtUID.setText(uid)
+        self.txtATR.setText(atr)
+        self.btnConnetPICC.setText("Connect")
+        self.tabMain.setEnabled(False)
+        self.is_picc_connect = False
+        self.write_statusbar("Card disconnected", "blue")
+
+    def reload_readers(self):
         self.cmbReader.clear()
         for reader in readers():
             reader_item = QtGui.QStandardItem(reader.name)
             reader_item.setData(reader)
             self.reader_model.appendRow(reader_item)
 
-    def _get_active_reader(self):
-        reader_idx = self.cmbReader.currentIndex()
-        reader_obj = self.reader_model.item(reader_idx)
-        if not hasattr(reader_obj, 'data'):
-            return None
-        return reader_obj.data()
-
-    def _connect(self):
-        try:
-            reader = self._get_active_reader()
-            if not reader:
-                self.current_connection = None
-                self.write_statusbar("No reader selected", "red")
-                return
-
-            self.current_connection = reader.createConnection()
-            self.current_connection.connect()
-            # get UID and ATR
-            uid = self.transmit(command.UID)
-            self.txtUID.setText(toHexString(uid))
-            self.txtATR.setText(toHexString(self.current_connection.getATR()))
-        except (NoCardException, CardConnectionException) as e:
-            self._reload_readers()
-            self.write_statusbar(str(e), "red")
-
     def transmit(self, cmd):
-        reader = self._get_active_reader()
-        if not reader:
-            self.current_connection = None
-            return
-        if not self.current_connection:
-            self._connect()
-
         data, sw1, sw2 = self.current_connection.transmit(toBytes(cmd))
+        # write status to statusbar
         status_code = toHexString([sw1, sw2])
-
         status_color = "green" if status_code == '90 00' else "red"
         self.write_statusbar(status_code, status_color)
+        return toHexString(data)
 
-        return data
+    def write_statusbar(self, message, color='green'):
+        self.statusbar.setStyleSheet("color: %s;" % color)
+        self.statusbar.showMessage(message)
 
 
 def main():
@@ -129,4 +146,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
