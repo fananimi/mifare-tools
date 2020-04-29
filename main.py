6a1d5bf9212b87c8a7f5def8bfcc170163784a2c
Fanani M. Ihsan <fanani.mi@gmail.com>
2020-04-29 13:16:13 +0700

    add button action

--- main.py
+++ main.py
@@ -9,6 +9,42 @@ class MifareTools(Ui_MainWindow, QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MifareTools, self).__init__(parent)
         self.setupUi(self)
+        self._register_signal()
+
+    # --------------------------------------------------------------------------------
+    # ****************** All signals must register on this section ******************|
+    # --------------------------------------------------------------------------------
+    def _register_signal(self):
+        self.btnReloadReader.clicked.connect(self.on_click_button)
+        self.btnConnetPICC.clicked.connect(self.on_click_button)
+        self.btnAuthKeyA.clicked.connect(self.on_click_button)
+        self.btnAuthKeyB.clicked.connect(self.on_click_button)
+        self.btnFactoryKeyA.clicked.connect(self.on_click_button)
+        self.btnFactoryKeyB.clicked.connect(self.on_click_button)
+
+    def on_click_button(self):
+        btnID = self.sender().objectName()
+        if btnID == 'btnReloadReader':
+            print("btnReloadReader")
+            return
+        elif btnID == "btnConnetPICC":
+            print("btnConnetPICC")
+            return
+        elif btnID == "btnReloadReader":
+            print("btnReloadReader")
+            return
+        elif btnID == "btnAuthKeyA":
+            print("btnAuthKeyA")
+            return
+        elif btnID == "btnAuthKeyB":
+            print("btnAuthKeyB")
+            return
+        elif btnID == "btnFactoryKeyA":
+            print("btnFactoryKeyA")
+            return
+        elif btnID == "btnFactoryKeyB":
+            print("btnFactoryKeyB")
+            return
 
 
 def main():
